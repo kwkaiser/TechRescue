b193a86e56406499f6c484a2770c8b1f83e99748
kwkaiser <kwkaiser@uvm.edu>
2019-07-05 14:30:44 -0400

    Removed unused notification messages

--- anacron_script.sh
+++ anacron_script.sh
@@ -8,13 +8,11 @@ kdialog --passivepopup "This may slow down the device for the next 5-10 minutes"
 # Clone package, add relevant username and password
 git clone https://github.com/kwkaiser/TechRescue.git /home/USER/TechRescue
 
-sed -i 's/$USR/$NEWU/g' /home/USER/TechRescue/automate_upload.sh
-sed -i 's/$PASSWORD/$NEWP/g' /home/USER/TechRescue/automate_upload.sh
+sed -i 's/$USR/NEWU/g' /home/USER/TechRescue/automate_upload.sh
+sed -i 's/$PASSWORD/NEWP/g' /home/USER/TechRescue/automate_upload.sh
 
 # Notify user and build
-notify-send "Beginning to run Tech Rescue build script; this may take up some resources"
 sh /home/USER/TechRescue/automate_upload.sh
-notify-send "Tech Rescue build finished"
 
 # Remove remaining files  & cleanup
 rm -rf /home/USER/TechRescue/
