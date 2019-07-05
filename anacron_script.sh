9c31b801ff9806fa32343778215ccf2b0d537f72
kwkaiser <kwkaiser@uvm.edu>
2019-07-05 12:39:55 -0400

    Syntax fix

--- anacron_script.sh
+++ anacron_script.sh
@@ -4,8 +4,8 @@
 # Clone package, add relevant username and password
 git clone https://github.com/kwkaiser/TechRescue.git /home/$USER/TechRescue
 
-sed -i 's/"$USR"/"$NEWUSR"/g' /home/$USER/TechRescue/automate_upload.sh
-sed -i 's/"$PASSWORD"/"$NEWPASSWORD"/g' /home/$USER/TechRescue/automate_upload.sh
+sed -i 's/"$USR"/"$NEWUSR"/g' /home/USER/TechRescue/automate_upload.sh
+sed -i 's/"$PASSWORD"/"$NEWPASSWORD"/g' /home/USER/TechRescue/automate_upload.sh
 
 # Notify user and build
 notify-send "Beginning to run Tech Rescue build script; this may take up some resources"
