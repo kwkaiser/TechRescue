5563bd5848f6bf55273b27d7247276852fc9ef4b
kwkaiser <kwkaiser@uvm.edu>
2019-07-05 14:09:24 -0400

    Fixed syntax error in anacron script

--- anacron_script.sh
+++ anacron_script.sh
@@ -8,8 +8,8 @@ kdialog --passivepopup "This may slow down the device for the next 5-10 minutes"
 # Clone package, add relevant username and password
 git clone https://github.com/kwkaiser/TechRescue.git /home/$USER/TechRescue
 
-sed -i 's/"$USR"/"$NEWUSR"/g' /home/USER/TechRescue/automate_upload.sh
-sed -i 's/"$PASSWORD"/"$NEWPASSWORD"/g' /home/USER/TechRescue/automate_upload.sh
+sed -i 's/$USR/$NEWUSR/g' /home/USER/TechRescue/automate_upload.sh
+sed -i 's/$PASSWORD/$NEWPASSWORD/g' /home/USER/TechRescue/automate_upload.sh
 
 # Notify user and build
 notify-send "Beginning to run Tech Rescue build script; this may take up some resources"
