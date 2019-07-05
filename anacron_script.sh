62d201230445389c59100fe55029ffc6760c24c5
kwkaiser <kwkaiser@uvm.edu>
2019-07-05 14:16:37 -0400

    More bug fixes

--- anacron_script.sh
+++ anacron_script.sh
@@ -6,10 +6,10 @@ kdialog --passivepopup "The Tech Rescue build script is about to be running, ple
 kdialog --passivepopup "This may slow down the device for the next 5-10 minutes" 500
 
 # Clone package, add relevant username and password
-git clone https://github.com/kwkaiser/TechRescue.git /home/$USER/TechRescue
+git clone https://github.com/kwkaiser/TechRescue.git /home/USER/TechRescue
 
-sed -i 's/$USR/$NEWUSR/g' /home/USER/TechRescue/automate_upload.sh
-sed -i 's/$PASSWORD/$NEWPASSWORD/g' /home/USER/TechRescue/automate_upload.sh
+sed -i 's/$USR/$NEWU/g' /home/USER/TechRescue/automate_upload.sh
+sed -i 's/$PASSWORD/$NEWP/g' /home/USER/TechRescue/automate_upload.sh
 
 # Notify user and build
 notify-send "Beginning to run Tech Rescue build script; this may take up some resources"
--- configure.sh
+++ configure.sh
@@ -6,4 +6,14 @@ read COMPNAME
 sed -i "s/USER/$COMPNAME/g" automate_upload.sh
 sed -i "s/USER/$COMPNAME/g" anacron_script.sh
 
+# Replace $NEWU in anacron_script.sh
+echo "Enter the name of the account for the share drive"
+read NEWU
+sed -i "s/NEWU/$NEWU/g" anacron_script.sh
+
+# Replace $NEWP in anacron_script.sh
+echo "Enter the password of the account for the share drive"
+read NEWP
+sed -i "s/NEWP/$NEWP/g" anacron_script.sh
+
 echo "Now enter the updated credentials for the relevant servershare to the automate_upload.sh script"
